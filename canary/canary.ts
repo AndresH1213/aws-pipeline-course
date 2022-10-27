523ac2e63a26f9187c97c475f14c92265f56e583
Andres Hernandez <edanhebla1213@hotmail.com>
2022-10-26 23:29:32 -0500

    removing undefined log function

--- canary/canary.ts
+++ canary/canary.ts
@@ -1,12 +1,10 @@
 const synthetics = require('Synthetics');
-const log = require('SyntheticsLogger');
 
 const canary = async function () {
   await synthetics.executeHttpStep(
     'Verify API return succesful response',
     process.env.API_ENDPOINT
   );
-  log('http executed...');
 };
 
 exports.handler = async () => {
